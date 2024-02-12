96ed91fcaa8749ff07e0945f3a6b7cf77d80da2c
ruthweng <ruthweng333@gmail.com>
2024-02-12 12:50:58 -0500

    mk

---
+++ Vue/src/components/icons/DestCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+<h2>{{ Destination.name }}</h2>
+<img :src="Destination.img" alt="" />
+<h3>{{ clicked }}</h3>
+<button @clicked="increment">Click Me</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const props = defineProps({
+  Destination: Object,
+});
+
+//clicker logic
+const clicked = ref(0);
+function increment(){
+  clicked.value++;
+}
+
+</script>
+
+<style scoped>
+img {
+  width: 300px;
+  height: 350px;
+
+}
+</style>
