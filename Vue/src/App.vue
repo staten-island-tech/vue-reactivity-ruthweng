32aa6694d33bc0ae48061e6f9e8e4085c5255de7
ruthweng <138687656+ruthweng@users.noreply.github.com>
2024-03-04 07:23:14 -0500

    hi

--- Vue/src/App.vue
+++ Vue/src/App.vue
@@ -2,15 +2,15 @@
   <div id="app">
   <section id="userInput">
     <h1 id="header">A Very Cheesy Store!</h1>
-    <div class="milk-types">
+    <div class="cheese-types">
     <Card
       class="type"
-      v-for="(milkType, index) in milkTypes"
-      @addMilk="addMilk(index)"
+      v-for="(cheeseType, index) in cheeseTypes"
+      @addCheese="addCheese(index)"
       :key="index"
-      :milk="milkType.milk"
-      :img="milkType.img"
-      :price="milkType.price"
+      :cheese="cheeseType.cheese"
+      :img="cheeseType.img"
+      :price="cheeseType.price"
     />
   </div>
   </section>
@@ -19,14 +19,14 @@
     <h1>Your Cart</h1>
     <Cart
       class="cart"
-      v-for="(milkType, index) in order"
+      v-for="(cheeseType, index) in order"
       :key="index"
-      :order="milkType.milk"
-      :price="milkType.price"
+      :order="cheeseType.cheese"
+      :price="cheeseType.price"
     />
     <h2 class = subtotal>Subtotal: ${{ subtotal }}</h2>
     <button class="buttons" @click="remove()">Remove Last Chez</button>
-    <button class="buttons" @click="removeAllMilk()">Remove All Cheeses</button>
+    <button class="buttons" @click="removeAllCheese()">Remove All Cheeses</button>
     </section>
   </div>
 </template>
@@ -35,7 +35,7 @@
 import Card from "./components/CheeseCard.vue";
 import Cart from "./components/CheeseCart.vue";
 export default {
-  milk: "App",
+  cheese: "App",
   components: {
     Card,
     Cart,
@@ -43,80 +43,80 @@ export default {
   data() {
   return {
     subtotal: 0,
-    selectedMilk: 0,
-    milkTypes: [
+    selectedCheese: 0,
+    cheeseTypes: [
        {
-         milk: "Cottage",
+         cheese: "Cottage",
          price: 1300,
          img: "https://cheesemaking.com/cdn/shop/products/cottage-cheese-1_grande.jpg?v=1529434175",
        },
        {
-         milk: "Cream",
+         cheese: "Cream",
          price: 800,
          img: "https://upload.wikimedia.org/wikipedia/commons/f/f7/Philly_cream_cheese.jpg",
        },
        {
-         milk: "Mozzarella",
+         cheese: "Mozzarella",
          price: 1200,
          img: "https://www.seriouseats.com/thmb/0LrG8tB4BkzQarr2fqrpykcaDBg=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__serious_eats__seriouseats.com__recipes__images__2015__10__20151017-pies-vicky-wasik-2-6f491edb6065485a86d6af639a592298.jpg",
        },
        {
-         milk: "Bel paese",
+         cheese: "Bel paese",
          price: 900,
          img: "https://m.media-amazon.com/images/I/41kT2d78y5L.jpg",
        },
        {
-         milk: "Brie",
+         cheese: "Brie",
          price: 400,
          img: "https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fill,g_center,w_730,h_913/k%2FPhoto%2FSeries%2F2023-11-how-to-eat-brie%2Fhow-to-eat-brie-381",
        },
        {
-         milk: "Camembert",
+         cheese: "Camembert",
          price: 800,
          img: "https://marinfrenchcheese.com/wp-content/uploads/2021/10/Product-Page_Petite-Camembert_Beauty-Shot.jpg",
        },
        {
-         milk: "Cheddar",
+         cheese: "Cheddar",
          price: 800,
          img: "https://shop.burnettdairy.com/cdn/shop/products/BD_MildChed.jpg?v=1589903792",
        },
        {
-         milk: "Cheshire",
+         cheese: "Cheshire",
          price: 800,
          img: "https://www.gourmetdash.com/media/catalog/product/cache/8794a98778c2ca77788a2eb8b07fbd40/1/0/1050_beauty_so9nea2ke2blirmu_1.jpg",
        },
        {
-         milk: "Derby",
+         cheese: "Derby",
          price: 800,
          img: "https://s3.us-east-2.amazonaws.com/cheesemaking-supply-co/014bd94fee1e456d98c98a158d90b6dc.jpg",
        },
        {
-         milk: "Parmesan",
+         cheese: "Parmesan",
          price: 800,
          img: "https://frankenmuthcheesehaus.com/cdn/shop/products/PXL_20210430_200530224.PORTRAIT_1019x700.jpg?v=1619980804",
        },
        {
-         milk: "Pecorino Romano",
+         cheese: "Pecorino Romano",
          price: 800,
          img: "https://www.cheese.com/media/img/cheese/Pecorino_romano_cheese.jpg",
        },
        {
-         milk: "Caciocavallo",
+         cheese: "Caciocavallo",
          price: 800,
          img: "https://www.cheese.com/media/img/cheese/Caciocavallo.jpg",
        },
        {
-         milk: "Danish Blue",
+         cheese: "Danish Blue",
          price: 800,
          img: "https://dorothy-lane-market.s3.us-east-2.amazonaws.com/shop/items/danish-blue-1_662x450.JPEG",
        },
        {
-         milk: "Dorset Blue",
+         cheese: "Dorset Blue",
          price: 800,
          img: "https://upload.wikimedia.org/wikipedia/commons/6/67/Dorset_Blue_Vinney_cheese.jpg",
        },
        {
-         milk: "Gorgonzola",
+         cheese: "Gorgonzola",
          price: 800,
          img: "https://www.cheese.com/media/uploads/blog/2023/05/gorgonzola.jpg",
        },
@@ -127,10 +127,10 @@ export default {
   };
   },
 methods: {
-  addMilk(index) {
-    this.order.push(this.milkTypes[index]);
-    this.orderPrice.push(this.milkTypes[index].price);
-    this.subtotal = this.subtotal + this.milkTypes[index].price;
+  addCheese(index) {
+    this.order.push(this.cheeseTypes[index]);
+    this.orderPrice.push(this.cheeseTypes[index].price);
+    this.subtotal = this.subtotal + this.cheeseTypes[index].price;
 },
   remove() {
   if (this.order.length !== 0) {
@@ -140,7 +140,7 @@ methods: {
     this.orderPrice.pop();
   }
 },
-  removeAllMilk() {
+  removeAllCheese() {
     this.order = [];
     this.orderPrice = [];
     this.subtotal = 0;
@@ -191,7 +191,7 @@ methods: {
   flex-direction: column;
   font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
 }
-.milk-types {
+.cheese-types {
   display: flex;
   justify-content: center;
   flex-wrap: wrap;
